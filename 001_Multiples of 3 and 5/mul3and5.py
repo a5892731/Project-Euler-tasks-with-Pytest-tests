6cb3313bf5ce6f3b3c4f7d0ac1464788b3235560
a5892731 <a5892731@o2.pl>
2021-01-20 19:13:30 +0100

    create mul3and5 class

---
+++ 001_Multiples of 3 and 5/mul3and5.py
@@ -0,0 +1,54 @@
+'''
+author: a5892731
+date: 2021-01-30
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+__________________________________________
+try to run it on Git Bash :)
+
+'''
+
+
+
+class Multiples_3_and_5(object):
+    version = "1.00"
+    div_range = 1000
+
+    def __init__(self):
+        self.sum_of_dividers = 0
+        self.dividers = ()
+
+        self.find_dividers_of_3_and_5(self.div_range)
+        self.find_sum_of_dividers()
+
+        #print("Sum off dividers list is: {}".format(self.sum_of_dividers))
+
+    def find_dividers_of_3_and_5(self, div_range):
+        output =  [number for number in range(div_range)
+                if number % 3 == 0 and number % 5 == 0]
+        self.dividers = output
+        #print("Dividors of 3 and 5 in range {} are:\n {}".format(div_range, output))
+
+    def find_sum_of_dividers(self):
+        sum = 0
+        for divider in self.dividers:
+            sum += divider
+        self.sum_of_dividers = sum
+        #print("Sum off dividers list is: {}".format(sum))
+
+    def clear(self):
+        self.sum_of_dividers = 0
+        self.dividers = ()
+
+#------------------------------------------------------------------
+
+
+
+#output = Multiples_3_and_5()
+#print("\nSum off dividers list is: {} in range {}".format(output.sum_of_dividers, output.div_range))
+#output.div_range = 100
+#output.__init__()
+#print("\nSum off dividers list is: {} in range {}".format(output.sum_of_dividers, output.div_range))
---
+++ 001_Multiples of 3 and 5/mul3and5_test.py
@@ -0,0 +1,18 @@
+'''
+author: a5892731
+date: 2021-01-30
+
+This is test file for mul3and5 module
+
+_________________________
+
+Remember to install:
+
+pip install pytest
+
+'''
+
+import pytest
+
+from mul3and5 import Multiples_3_and_5
+
