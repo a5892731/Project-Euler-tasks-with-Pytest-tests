0774c4db2c7237cfb634e41267425056dd3cd238
a5892731 <a5892731@o2.pl>
2021-01-20 19:58:49 +0100

    create mul3and5 class

--- 001_Multiples of 3 and 5/mul3and5.py
+++ 001_Multiples of 3 and 5/mul3and5.py
@@ -19,24 +19,25 @@ class Multiples_3_and_5(object):
 
     def __init__(self):
         self.sum_of_dividers = 0
-        self.dividers = ()
+        self.dividers = []
 
-        self.find_dividers_of_3_and_5(self.div_range)
-        self.find_sum_of_dividers()
+        self.dividers = self.find_dividers_of_3_or_5(self.div_range)
+        self.sum_of_dividers = self.find_sum_of_dividers()
 
         #print("Sum off dividers list is: {}".format(self.sum_of_dividers))
 
-    def find_dividers_of_3_and_5(self, div_range):
-        output =  [number for number in range(div_range)
-                if number % 3 == 0 and number % 5 == 0]
-        self.dividers = output
-        #print("Dividors of 3 and 5 in range {} are:\n {}".format(div_range, output))
+    def find_dividers_of_3_or_5(self, div_range):
+        output =  [number for number in range(div_range + 1)
+                if (number % 3 == 0 or number % 5 == 0) and number != 0]
+
+        #print(">>>> Dividors of 3 and 5 in range {} are:\n {}".format(div_range, output))
+        return output
 
     def find_sum_of_dividers(self):
         sum = 0
         for divider in self.dividers:
             sum += divider
-        self.sum_of_dividers = sum
+        return sum
         #print("Sum off dividers list is: {}".format(sum))
 
     def clear(self):
@@ -46,9 +47,9 @@ class Multiples_3_and_5(object):
 #------------------------------------------------------------------
 
 
-
 #output = Multiples_3_and_5()
 #print("\nSum off dividers list is: {} in range {}".format(output.sum_of_dividers, output.div_range))
-#output.div_range = 100
+#output.div_range = 3
 #output.__init__()
 #print("\nSum off dividers list is: {} in range {}".format(output.sum_of_dividers, output.div_range))
+#print("Dividors of 3 or 5 in range {} are:\n{}".format(output.div_range, output.dividers))
--- 001_Multiples of 3 and 5/mul3and5_test.py
+++ 001_Multiples of 3 and 5/mul3and5_test.py
@@ -10,9 +10,28 @@ Remember to install:
 
 pip install pytest
 
+
+
 '''
 
 import pytest
 
 from mul3and5 import Multiples_3_and_5
 
+
+def multiples_3_and_5_initialization():
+    multiples_3_and_5 = Multiples_3_and_5()
+    assert multiples_3_and_5
+
+
+@pytest.mark.parametrize("div_range, expected",[
+    (3, [3]),
+    (5, [3, 5]),
+    (10, [42])])
+
+def test_find_dividers_of_3_or_5(div_range, expected):
+    multiples_3_and_5 = Multiples_3_and_5()
+    multiples_3_and_5.div_range = div_range
+    multiples_3_and_5.__init__()
+    print(">>> >>>RANGE {} DIVIDERS LIST: {}".format(div_range, multiples_3_and_5.dividers))
+    assert multiples_3_and_5.dividers == expected
