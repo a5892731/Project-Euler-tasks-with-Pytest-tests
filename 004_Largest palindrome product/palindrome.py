20870cb04a3eee5a86734722c36964bd85179867
a5892731 <a5892731@o2.pl>
2021-01-24 16:07:47 +0100

    004

--- 004_Largest palindrome product/palindrome.py
+++ 004_Largest palindrome product/palindrome.py
@@ -7,16 +7,28 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 '''
 
-def palindromic_num_from_mul_of_two_num(num_1 = 999, num_2 = 999):
+def palindromic_num_from_mul_of_two_num(num_1 =999, num_2 = 999):
+    output_list = [1]
+    min_value = 1
+    if min_value > 1000:
+        min_value = num_1 * num_2 / 100 * 50 # 50%
+    elif min_value > 10000:
+        min_value = num_1 * num_2 / 100 * 80 # 70%
+    elif min_value > 100000:
+        min_value = num_1 * num_2 / 100 * 90 # 90%
+
 
     for num1 in range(num_1, 0, -1):
         for num2 in range(num_2, 0, -1):
             output = num1 * num2
             if str(output) == str(output)[::-1]:
-                output
+                output_list.append(output)
+                break
+            if output < min_value: # optimalization: if mul operation value is less than XX % of max then break
+                break
 
-    return output
+    return max(output_list)
 
 
 
-print(palindromic_num_from_mul_of_two_num())
+#print(palindromic_num_from_mul_of_two_num(99, 99))
--- 004_Largest palindrome product/palindrome_test.py
+++ 004_Largest palindrome product/palindrome_test.py
@@ -18,25 +18,37 @@ import pytest
 
 from palindrome import palindromic_num_from_mul_of_two_num
 
-def test_palindromic_num_from_mul_of_two_num(num_1 = 999, num_2 = 999):
-    output_list = []
+def max_palindromic_num_test(num_1, num_2):
+    output_list = [1]
     for num1 in range(num_1, 0, -1):
         for num2 in range(num_2, 0, -1):
             output = num1 * num2
             if str(output) == str(output)[::-1]:
                 output_list.append(output)
 
-    return max(output_list)
+    return max(output_list)  # check max for all posibilities
 
 
-@pytest.mark.parametrize("number, expected",[
-    (number, test_palindromic_num_from_mul_of_two_num(number)) for number in range(1, 1000)
+@pytest.mark.parametrize("number, number2, expected",[
+    (number, number, max_palindromic_num_test(number, number)) for number in range(1, 200)
 ])
 
 
-def test_largest_prime_factor_from_number(number, expected):
+def test_palindromic_num_from_mul_of_two_num(number, number2, expected):
 
-    output = largest_prime_factor_from_number(number)
-    print(">>> >>> range {} max prime number: {}".format(number, output))
+    output = palindromic_num_from_mul_of_two_num(number, number2)
+    print(">>> >>> range {} and {} max palindromic number: {} expected {}".format(number,number2, output, expected))
     assert output == expected
 
+
+
+@pytest.mark.parametrize("number, number2, expected",[
+    (number, number, max_palindromic_num_test(number, number)) for number in range(800, 1000)
+])
+
+
+def test_palindromic_num_from_mul_of_two_num2(number, number2, expected):
+
+    output = palindromic_num_from_mul_of_two_num(number, number2)
+    print(">>> >>> range {} and {} max palindromic number: {} expected {}".format(number, number2, output, expected))
+    assert output == expected
